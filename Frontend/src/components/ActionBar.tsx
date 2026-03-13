0080b3107ed2499588772d3f465bdd7037c19e2d
Pierre ROMAN <pierre.roman.dev@gmail.com>
2026-03-13 13:56:23 +0100

    feat(select all): add a check box to select all users

--- Frontend/src/components/ActionBar.tsx
+++ Frontend/src/components/ActionBar.tsx
@@ -1,50 +1,112 @@
 import styles from "./ActionBar.module.css";
 import trashIcon from "../assets/icons/trash-2.svg";
 import copyIcon from "../assets/icons/copy.svg";
-import minusIcon from "../assets/icons/square-minus.svg";
+import { CheckBox } from "./ui/CheckBox";
+import { Button } from "./ui/Button";
 
 type ActionBarProps = {
   selectedCount: number;
+  totalCount: number;
   onDuplicate: () => void;
   onDelete: () => void;
+  onSelectAll: () => void;
   onClearSelection: () => void;
 };
 
+type SelectAllCheckboxLabelProps = {
+  selectedCount: number;
+  totalCount: number;
+  onClearSelection: () => void;
+  onSelectAll: () => void;
+};
+
+function SelectAllCheckboxLabel({
+  selectedCount,
+  totalCount,
+  onClearSelection,
+  onSelectAll,
+}: SelectAllCheckboxLabelProps) {
+  const allSelected = selectedCount === totalCount && totalCount > 0;
+  let labelText;
+  if (selectedCount > 0) {
+    labelText = (
+      <>
+        {selectedCount} element{selectedCount > 1 && "s"} selected
+        {!allSelected && (
+          <>
+            {" "}
+            -{" "}
+            <Button
+              variant="link"
+              onClick={(event) => {
+                onSelectAll();
+                event.preventDefault();
+              }}
+            >
+              Select all
+            </Button>
+          </>
+        )}
+      </>
+    );
+  } else {
+    labelText = (
+      <>
+        Select all ({totalCount} element{totalCount > 1 && "s"})
+      </>
+    );
+  }
+
+  return (
+    <span className={styles.counter}>
+      <CheckBox
+        id="selectAllCheckBox"
+        variant={allSelected ? "default" : "minus"}
+        checked={selectedCount > 0}
+        onChange={() =>
+          selectedCount > 0 ? onClearSelection() : onSelectAll()
+        }
+      />
+      <label htmlFor="selectAllCheckBox">{labelText}</label>
+    </span>
+  );
+}
+
 export function ActionBar({
   selectedCount,
+  totalCount,
   onDuplicate,
   onDelete,
+  onSelectAll,
   onClearSelection,
 }: ActionBarProps) {
   const hasSelection = selectedCount > 0;
 
   return (
     <div className={styles.container}>
-      <span className={styles.counter}>
-        <button
-          className={styles.button}
-          onClick={onClearSelection}
-          disabled={!hasSelection}
-        >
-          <img src={minusIcon} alt="" width="20" height="20" />
-        </button>
-        {selectedCount} elements selected
-      </span>
+      <SelectAllCheckboxLabel
+        selectedCount={selectedCount}
+        totalCount={totalCount}
+        onSelectAll={onSelectAll}
+        onClearSelection={onClearSelection}
+      />
       <div className={styles.actions}>
-        <button
+        <Button
+          variant="ghost"
           className={styles.button}
           onClick={onDuplicate}
           disabled={!hasSelection}
         >
           <img src={copyIcon} alt="" width="20" height="20" />
-        </button>
-        <button
+        </Button>
+        <Button
+          variant="ghost"
           className={styles.button}
           onClick={onDelete}
           disabled={!hasSelection}
         >
           <img src={trashIcon} alt="" width="20" height="20" />
-        </button>
+        </Button>
       </div>
     </div>
   );
